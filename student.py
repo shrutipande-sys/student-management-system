b77b524d5e1935496707dea51702ae1326c26f15
https://github.com/shrutipande-sys <https://github.com/shrutipande-sys/student-management-system.git>
2026-07-17 10:31:48 +0530

    modified search_student function

--- student.py
+++ student.py
@@ -6,6 +6,7 @@ def add_student(id, name):
     return True
 
 
+
 def remove_student(id):
     if id in students:
         del students[id]
@@ -14,8 +15,10 @@ def remove_student(id):
 
 
 def search_student(id):
-    return students.get(id)
-
+    if id in students:
+        return students[id]
+    else:
+        return "Student Not Found"
 
 def update_student(id, name):
     if id in students:
